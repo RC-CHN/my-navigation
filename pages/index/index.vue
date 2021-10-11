55e8db5a4b8acb70faaa0e267e9abf37068af2a2
Ruochen Pan <panruochen@rcfortress.site>
2021-10-11 15:50:49 +0800

    从global中获取站点列表

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -27,56 +27,12 @@
 			return {
 				title: 'Hello',
 				searchText: "",
-				sites: [{
-						'url': "https://cas.hdu.edu.cn/",
-						'title': '数字杭电',
-						'icon': '../../static/web_icons/HDU.png'
-					},
-					{
-						'url': "https://gitee.com",
-						'title': 'Gitee',
-					},
-					{
-						'url': "https://github.com/",
-						'title': 'Github',
-					},
-					{
-						'url': "https://www.bilibili.com",
-						'title': "Bilibili"
-					},
-					{
-						'url': "http://m.rcfortress.site:996/#/",
-						'title': "ybb计算器",
-						'icon': "../../static/favicon.png"
-					},
-					{
-						'url': "http://m.rcfortress.site:81/login",
-						'title': "Unraid管理页面",
-						'icon': "../../static/web_icons/unraid.png"
-					},
-					{
-						'url': "http://m.rcfortress.site:5000/",
-						'title': "群晖-杭州",
-						'icon': "../../static/web_icons/DSM.png",
-					},
-					{
-						'url': "https://nas.rcfortress.site:5001/",
-						'title': "群晖-湖州",
-						'icon': "../../static/web_icons/DSM.png"
-					},
-					{
-						'url': "http://m.rcfortress.site:9080",
-						'title': "Gitlab",
-					},
-					{
-						'url': "http://m.rcfortress.site:8443/login",
-						'title': "CodeServer",
-						'icon': "../../static/web_icons/code.png"
-					}
-				]
+				sites: null
 			}
 		},
-		onLoad() {},
+		onLoad() {
+			this.$data.sites = getApp().globalData.sites
+		},
 		methods: {
 			searchLog() {
 				console.log(this.$data.searchText)
